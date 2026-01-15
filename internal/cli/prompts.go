5d8f1b7f9250740518ce5363c6f9c666d54d9982
Youssef <mac@192.168.1.6>
2026-01-15 15:41:00 +0200

    added a new region

--- internal/cli/prompts.go
+++ internal/cli/prompts.go
@@ -177,7 +177,7 @@ func getRegion() (string, error) {
 		Items: []string{
 			"us-east-1", "us-east-2", "us-west-1", "us-west-2",
 			"eu-west-1", "eu-west-2", "eu-central-1",
-			"ap-southeast-1", "ap-southeast-2", "ap-northeast-1",
+			"ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "me-central-1",
 		},
 		Templates: &promptui.SelectTemplates{
 			Label:    "{{ . | cyan }}?",
